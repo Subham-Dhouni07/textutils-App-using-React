93cb3a69310181964b3a1d5ec2e0a7aea2afdc8b
Subham Dhouni <55024488+Subham-Dhouni07@users.noreply.github.com>
2023-11-01 14:59:21 +0530

    update the logics and UI.
    update the logics and UI.

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 export default function Alert(props) {
   return (
-        props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+    <div style = {{height : '50px'}}>   
+    { props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
            <strong>{props.alert.msg}</strong>
-        </div>
-
+        </div>}
+    </div>
   )
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,15 +3,30 @@ import React, {useState} from 'react'
 export default function TextForm(props) {
 
   const handleUpClick = () =>{
-    let newText = text.toUpperCase();
-    setText(newText);
-    props.showAlert("Text converted to Uppercase!", "success")
+
+    if(text.length == 0)
+    {
+      props.showAlert("No text found!", "danger")
+    }
+    else
+    {
+      let newText = text.toUpperCase();
+      setText(newText);
+      props.showAlert("Text converted to Uppercase!", "success")
+    }
   }
   
   const handleLowClick = () =>{
-    let newText = text.toLowerCase();
-    setText(newText);
-    props.showAlert("Text converted to Lowercase!", "success")
+    if(text.length == 0)
+    {
+      props.showAlert("No text found!", "danger")
+    }
+    else
+    {
+      let newText = text.toLowerCase();
+      setText(newText);
+      props.showAlert("Text converted to Lowercase!", "success")
+    }
   }
   
   const handleClearClick = () =>{
@@ -31,22 +46,42 @@ export default function TextForm(props) {
   }
 
   const handleFirstLetter = () => {
-    let newText = text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
-    setText(newText);
-    props.showAlert("Text has been changed!", "success")
+    if(text.length == 0)
+    {
+      props.showAlert("No text found!", "danger")
+    }
+    else
+    {
+      let newText = text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+      setText(newText);
+      props.showAlert("Text has been changed!", "success")
+    }
   };
 
   const handleCopy = () => {
-    let text = document.getElementById("exampleFormControlTextarea1");
-    text.select();
-    navigator.clipboard.writeText(text.value);
-    props.showAlert("Copied!", "success")
+    if(text.length == 0)
+    {
+      props.showAlert("No text found!", "danger")
+    }
+    else
+    {
+      navigator.clipboard.writeText(text);
+      props.showAlert("Copied!", "success")
+    }
   };
 
   const handleExtraSpaces = ()=>{
-    let newText = text.replace(/\s+/g, ' ').trim();
-    setText(newText)
-    props.showAlert("Removed extra spaces!", "success")
+    if(text.length == 0)
+    {
+      props.showAlert("No text found!", "danger")
+    }
+    else
+    {
+
+      let newText = text.replace(/\s+/g, ' ').trim();
+      setText(newText)
+      props.showAlert("Removed extra spaces!", "success")
+    }
     }
 
   const [text, setText] = useState('');
@@ -58,17 +93,17 @@ export default function TextForm(props) {
             <div className="mb-3">
                 <textarea className="form-control" id="exampleFormControlTextarea1" value={text} style={{backgroundColor: props.mode==='dark' ? '#6e8ba4' : 'white', color: props.mode==='dark' ? 'white' : 'black'}} onChange={handleOnChange} rows="8"></textarea>
             </div>
-            <button className="btn btn-success mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
-            <button className="btn btn-success mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
-            <button className="btn btn-success mx-2" onClick={handleClearClick}>Clear Text</button>
-            <button className="btn btn-success mx-2" onClick={speak}>Text Speech</button>
-            <button className="btn btn-success mx-2" onClick={handleFirstLetter}>Sentence Case</button>
-            <button className="btn btn-success mx-2" onClick={handleCopy}>Copy Text</button>
-            <button className="btn btn-success mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+            <button className="btn btn-success mx-2 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-success mx-2 my-1" onClick={handleLowClick}>Convert to LowerCase</button>
+            <button className="btn btn-success mx-2 my-1" onClick={handleClearClick}>Clear Text</button>
+            <button className="btn btn-success mx-2 my-1" onClick={speak}>Text Speech</button>
+            <button className="btn btn-success mx-2 my-1" onClick={handleFirstLetter}>Sentence Case</button>
+            <button className="btn btn-success mx-2 my-1" onClick={handleCopy}>Copy Text</button>
+            <button className="btn btn-success mx-2 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
         <div className="contianer my-4" style={{color: props.mode==='dark' ? 'white' : 'black'}}>
           <h3>Your Text Summary</h3>
-          <h6>{text.length} Characters ,  {text.split(" ").length} Words</h6>
+          <h6>{text.split(/\s+/).filter((element)=>{return element.length!== 0}).length } Words and {text.length} Characters</h6>
           <h3>Preview</h3>
           <p>{text.length > 0 ? text : "Enter something in the text box to preview"}</p>
         </div>
